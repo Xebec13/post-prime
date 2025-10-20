553f4a4dc373f4c41edca0d79a17456f12635fa6
Xebec13 <dhoesen@gmail.com>
2025-10-20 14:11:39 +0200

    feat: finalize Logo component with parallax animation and dynamic background sync

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,4 +1,6 @@
-import Logo from "../styles/Logo"
+"use client";
+import { useState, useEffect } from "react";
+import Logo from "../styles/Logo";
 
 const heroContent = {
   title: ["Post", "Prime"],
@@ -9,30 +11,43 @@ const heroContent = {
   ],
   year: "2020",
 };
+
 export default function HeroSection() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // 🔸 Gdy scroll > 800px → zmieniamy kolor tła
+      setIsScrolled(window.scrollY > 500);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section
       id="home"
-      className="h-[200vh] bg-orange-700 flex flex-col gap-6 p-3 sm:p-6"
+      className={`h-[150vh] flex flex-col gap-6 p-3 sm:p-6 transition-colors duration-700 ${
+        isScrolled ? "bg-black" : "bg-orange-700"
+      }`}
     >
-
       {/* === MAIN TITLE === */}
-      <div className="min-h-screen translate-y-1/6">
-        <div className=" grid grid-cols-2 gap-1 text-[clamp(1.5rem,15vw,20rem)] font-black uppercase text-nowrap text-transparent logo-text -z-50">
-          <h1
-            className="text-left bg-gradient-to-bl from-gray-200 to-gray-300 bg-clip-text scale-y-260">
+      <div className="min-h-screen translate-y-1/6 relative">
+        <div className="grid grid-cols-2 gap-1 text-[clamp(1.5rem,15vw,20rem)] font-black uppercase text-nowrap text-transparent -z-50">
+          <h1 className="text-left bg-gradient-to-bl from-gray-200 to-gray-300 bg-clip-text scale-y-260">
             {heroContent.title[0]}
           </h1>
           <h1 className="text-right bg-gradient-to-br from-gray-200 to-gray-300 bg-clip-text scale-y-260">
             {heroContent.title[1]}
           </h1>
-          {/* === ICONS (HAND + BALL) === */}
-        <Logo />
+
+          {/* === LOGO === */}
+          <Logo isScrolled={isScrolled} />
         </div>
 
         {/* === DESCRIPTION & YEAR === */}
         <div className="grid grid-cols-2 gap-1 uppercase mt-10 sm:mt-20">
-          {/* Left column - podcast description */}
           <div className="w-3/4 pl-1 sm:pl-2.5 text-left z-20">
             <h2 className="text-xl sm:text-3xl font-extrabold uppercase">
               {heroContent.subtitle}
@@ -44,7 +59,6 @@ export default function HeroSection() {
             ))}
           </div>
 
-          {/* Right column - year */}
           <p className="self-start pr-1 md:pr-2.5 text-right text-xs md:text-sm font-bold leading-none text-gray-300">
             {heroContent.year}
           </p>
